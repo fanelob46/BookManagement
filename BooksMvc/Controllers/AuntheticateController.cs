d2fec7753e2cd4571ee2e76f0b601e125ba84d72
fanelob46 <fanelob46@gmail.com>
2024-02-19 21:44:12 +0200

    Loging a user

--- BooksMvc/Controllers/AuntheticateController.cs
+++ BooksMvc/Controllers/AuntheticateController.cs
@@ -1,9 +1,16 @@
 ﻿using Azure.Identity;
 using BooksMvc.Models;
+using BooksMvc.Models.Aunthetication.Login;
 using BooksMvc.Models.Aunthetication.SignUp;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
 namespace BooksMvc.Controllers
 {
@@ -60,6 +67,46 @@ namespace BooksMvc.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError,
                        new Response { Status = "Error", Messange = "the role doesnt exist" });
             }
+        }
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginUser loginUser)
+        {
+            //checking the user
+            var user = await _userManager.FindByNameAsync(loginUser.Username);
+            if(user!=null && await _userManager.CheckPasswordAsync(user,loginUser.Password)) 
+            {
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+                var userRoles = await _userManager.GetRolesAsync(user);
+                foreach(var role in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, role));
+                }
+                var jwtToken = GetToken(authClaims);
+
+                return Ok(new { 
+                    token = new JwtSecurityTokenHandler().WriteToken(jwtToken),
+                    expiration = jwtToken.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+
+        private JwtSecurityToken GetToken(List<Claim> authClaims)
+        {
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddHours(1),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey,SecurityAlgorithms.HmacSha256));
+            return token;
         }
     }
 }
---
+++ BooksMvc/Models/Aunthetication/Login/LoginUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksMvc.Models.Aunthetication.Login
+{
+    public class LoginUser
+    {
+        [Required(ErrorMessage = "User Name is Required")]
+        public String? Username { get; set; }
+
+        [Required(ErrorMessage = "Password is Required")]
+        public String? Password { get; set; }
+    }
+}
